988fdb68fc9e2cd33c9e9b88987eccb6ca6980b9
toshunigam <toshuchandra@gmail.com>
2021-12-04 13:13:38 +0530

    appending content in files

--- files/file_ops1.py
+++ files/file_ops1.py
@@ -2,3 +2,8 @@ file = open("file_operation.txt", "w")
 file.write("This file is use for handling file operation")
 file.write("it allow us to wrtie it into a perticular file")
 file.close()
+
+file_append = open("file_operation_append.txt", "a")
+file_append.write("Hello there!! \n")
+file_append.write("This will append the file. \n")
+file_append.close()
