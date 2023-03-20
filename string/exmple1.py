e07adbfed59223efb62ab6018b2a393376251af4
toshunigam <toshuchandra@gmail.com>
2023-03-20 20:20:48 +0530

    working into string

--- string/exmple1.py
+++ string/exmple1.py
@@ -1,3 +1,8 @@
 first = 'toshu'
 last = "nigam"
 print(first + last) #there is no difference between single and double quotes in terms of string
+
+#make first letter capital
+camelcase = "this is for a camelcase testing"
+print(camelcase.upper())
+print(camelcase.capitalize())
