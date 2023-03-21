b94c7b65cfe23f11df05eb79c9ecf999d55df7f3
toshunigam <toshuchandra@gmail.com>
2023-03-21 21:07:48 +0530

    working into string camel case

--- string/exmple1.py
+++ string/exmple1.py
@@ -6,3 +6,11 @@ print(first + last) #there is no difference between single and double quotes in 
 camelcase = "this is for a camelcase testing"
 print(camelcase.upper())
 print(camelcase.capitalize())
+tmp = camelcase.split(' ')
+# print(tmp)
+arr = []
+i=0
+for val in tmp:
+    arr.insert(i, val.capitalize())
+    i = i + 1
+print(' '.join(arr))
