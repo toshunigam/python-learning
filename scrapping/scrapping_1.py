9ab8f56ed91ab243d5d58d5d86fac9d4118d7a73
toshunigam <toshuchandra@gmail.com>
2023-03-24 22:05:39 +0530

    working into scrapping

--- scrapping/scrapping_1.py
+++ scrapping/scrapping_1.py
@@ -2,27 +2,26 @@ import requests
 from bs4 import BeautifulSoup
  
 # Making a GET request
-r = requests.get('https://gventure.net/careers')
+r = requests.get('https://ayanshtechnology.com/contact.html')
  
 # check status code for response received
 # success code - 200
 print(r)
 
 # print content of request
-# print(r.content)
 # print(r.status_code)
 
 soup = BeautifulSoup(r.content, 'html.parser')
-print(soup.prettify())
-for link in soup.find_all('a'):
-    print(link.get('href'))
-print('hi there')
+# for link in soup.find_all('a'):
+    # print(link.get('href'))
+
 # Getting the title tag
 # print(soup.title)
 # print(soup.title.name)
-# s = soup.find('div', class_='JobsCard')
-# print(s)
-# content = s.find_all('h1')
+
+s = soup.find('div', class_="vp__address")
+
+content = s.find_all('p')
  
-# for line in content:
-    # print(line.text)
+for line in content:
+    print(line.text)
