175b65c85621204ae37166ef393f6878460ce311
toshunigam <toshuchandra@gmail.com>
2023-07-22 15:56:16 +0530

    string example

--- string/multiline.py
+++ string/multiline.py
@@ -7,3 +7,12 @@ print(a)
 
 b = 'Toshu Nigam'
 print(b[0],b[1])
+
+c = '''
+my name is bali sun jara manchali mera 
+dil chura ke tu kaha chali
+excuse me to pleae
+mujhe neend ab na aye meri jaan 
+chali na jaye
+'''
+print(c)
