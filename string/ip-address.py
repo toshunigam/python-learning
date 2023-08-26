c969d1cada765b51e99983b86422e1364a50cc68
toshunigam <toshuchandra@gmail.com>
2023-08-26 21:08:03 +0530

    function example

--- string/ip-address.py
+++ string/ip-address.py
@@ -13,3 +13,16 @@ for i in range(len(ipArrary)):
 
 print('You have entered ip addres is :',isValid)
     
+def validateIP(string):
+    ipArrary = string.split('.')
+    print(ipArrary)
+    isValid=''
+    for i in range(len(ipArrary)):
+        if int(ipArrary[i]) > 256:
+            isValid=False
+        else :
+            isValid=True
+
+    print('You have entered ip addres is :',isValid)
+
+validateIP("192.168.1.1")
